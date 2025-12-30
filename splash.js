ba6ded5c1678805c19131bdd17be54651ba2031f
Yasas0115 <pasindu.20240677@iit.ac.lk>
2025-12-30 17:25:33 +0530

    splash and login page done

--- splash.js
+++ splash.js
@@ -1,41 +1,31 @@
 import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 
 export default function SplashScreen({ navigation }) {
   useEffect(() => {
-    const timer = setTimeout(() => {
-      navigation.replace("Login"); // change if needed
+    const timeout = setTimeout(() => {
+      navigation.replace("Register");
     }, 2000);
 
-    return () => clearTimeout(timer);
+    return () => clearTimeout(timeout);
   }, [navigation]);
 
   return (
-    <LinearGradient
-      colors={["#6BBE45", "#3A7D2C"]} // gradient colors
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      style={styles.container}
-    >
-      {/* LOGO */}
+    <View style={styles.container}>
       <Image
         source={require("./assets/envotix-logo.png")}
         style={styles.logo}
       />
 
-      {/* APP NAME */}
       <View style={styles.titleContainer}>
         <Text style={styles.envo}>ENVO</Text>
         <Text style={styles.tix}>tix</Text>
       </View>
 
-      {/* TAGLINE */}
       <Text style={styles.subtitle}>Waste Management</Text>
-    </LinearGradient>
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -58,21 +48,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
-    envo: {
+  envo: {
     fontSize: 40,
     fontWeight: "bold",
     color: "#fff",
   },
-    tix: {
+  tix: {
     fontSize: 40,
     fontWeight: "bold",
     color: "#000",
-    marginLeft: 7,
+    marginLeft: 5,
   },
-
   subtitle: {
-    fontSize: 20,
-    color: "#000000ff",
+    fontSize: 18,
+    color: "#000",
     marginTop: 10,
   },
 });

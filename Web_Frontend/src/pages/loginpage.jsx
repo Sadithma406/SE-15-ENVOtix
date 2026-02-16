4639546d890ba3c939c779caefdd83b3e993a802
thishini20240748-dev <thishini.20240748@iit.ac.lk>
2026-02-16 16:58:07 +0530

    login page fetch url change

--- Web_Frontend/src/pages/loginpage.jsx
+++ Web_Frontend/src/pages/loginpage.jsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
     setMessage("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

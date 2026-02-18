85f7dc7e0a51e8b1ce18fa5d5d8f9418ac7a6d3d
thishini20240748-dev <thishini.20240748@iit.ac.lk>
2026-02-18 09:22:07 +0530

    did changes in  model path

--- Backend/routes/shopRoute.js
+++ Backend/routes/shopRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Shop = require("../models/Shop");
+const Shop = require("../models/shop");
 
 // GET all shops
 router.get("/", async (req, res) => {

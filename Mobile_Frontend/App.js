3e0afb02dc0824a60adab158442d5644f4db5972
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-02-24 10:33:03 +0530

    Route path changed

--- Mobile_Frontend/App.js
+++ Mobile_Frontend/App.js
@@ -23,7 +23,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Plastic" component={PlasticBinScreen} />
         <Stack.Screen name="SideBar" component={SideScreen} />

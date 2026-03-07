56e4259ad76f8fbba81b98e4129d8f0e6585f213
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-03-07 08:57:19 +0530

    signup page cliendID changed

--- Web_Frontend/src/main.jsx
+++ Web_Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Replace with your Google Client ID from Google Cloud Console
-const GOOGLE_CLIENT_ID = "813041258973-dn6jpf0um4kmategr507vg1up834j4t2.apps.googleusercontent.com"
+const GOOGLE_CLIENT_ID = "706653193337-94vk42aid000b9dmg6e8bv57moho75n0.apps.googleusercontent.com"
 
 // Note: StrictMode removed temporarily as it can cause issues with Leaflet maps
 createRoot(document.getElementById('root')).render(

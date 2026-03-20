ab20d9d26bf2055ff7f54316f28af66eae0a22f9
Your GitHub Name <laknidu.20240432@iit.ac.lk>
2026-03-20 17:01:56 +0530

    Css for background added

--- Mobile_Frontend/src/screens/Splash.js
+++ Mobile_Frontend/src/screens/Splash.js
@@ -34,6 +34,16 @@ export default function SplashScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   logo: {
     width: 140,
     height: 140,

0befdf7bd563ec59e12edccc357e4f4e033b9e88
Seniru <seniruemitha0203@gmail.com>
2026-03-21 19:53:32 +0530

    port 0.0.0.0 added

--- Backend/app.js
+++ Backend/app.js
@@ -31,4 +31,4 @@ app.use('/api/notifications', notificationRoutes);
 app.use('/api/lanebins', lanebinRoutes);
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));

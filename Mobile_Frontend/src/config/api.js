fbe328d1810298e5cbaaff07e3021a91f084f519
Seniru <seniruemitha0203@gmail.com>
2026-03-21 19:51:58 +0530

    API ip updated

--- Mobile_Frontend/src/config/api.js
+++ Mobile_Frontend/src/config/api.js
@@ -2,7 +2,7 @@
 // Find your IP: Run 'ipconfig' in terminal and look for IPv4 Address
 import { Platform } from 'react-native';
 
-const API_IP = '192.168.1.227';
+const API_IP = '10.168.7.103';
 const API_PORT = '5000';
 
 // Use localhost for web, IP address for mobile devices
